4d1128d2cc6abe4d9e8a88aa4bd7e9737b6c3d81
jatin-jangir <79351203+jatin-jangir@users.noreply.github.com>
2022-06-30 17:09:53 +0530

    Update dijastra.py

--- dijastra.py
+++ dijastra.py
@@ -1,17 +1,16 @@
 from time import sleep
-from tkinter import N, W
 import pygame
 import math
-import queue
+from queue import PriorityQueue
 
 WIDTH = 500
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption("Dijasktra Path Finding Algorithm")
+pygame.display.set_caption("Dijasktra Algorithm ")
 
 RED = (255, 0, 0) 
 GREEN = (0, 255, 0)
 BLUE = (0, 255, 0)
-YELLOW = (255, 255, 0)
+LGREY = (190, 190, 190)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 PURPLE = (128, 0, 128)
@@ -36,6 +35,9 @@ class Spot:
 	def is_closed(self):
 		return self.color == RED
 
+	def is_inQueue(self):
+		return self.color==LGREY
+
 	def is_open(self):
 		return self.color == GREEN
 
@@ -53,9 +55,7 @@ class Spot:
 
 	def make_start(self):
 		self.color = ORANGE
-	def make_complete(self):
-		self.color= YELLOW
-		
+
 	def make_closed(self):
 		self.color = RED
 
@@ -64,7 +64,8 @@ class Spot:
 
 	def make_barrier(self):
 		self.color = BLACK
-
+	def make_complete(self):
+		self.color= LGREY
 	def make_end(self):
 		self.color = TURQUOISE
 
@@ -98,48 +99,61 @@ def h(p1, p2):
 	return abs(x1 - x2) + abs(y1 - y2)
 
 
-def reconstruct_path(came_from, start, current, draw):
-    temp=current
-    while came_from[temp]!=start:
-        temp=came_from[temp]
-        temp.make_path()
-        draw()
-
-
+def reconstruct_path(came_from, current, draw):
+	while current in came_from:
+		current = came_from[current]
+		current.make_path()
+		draw()
+	current.make_start()
+	draw()
 
 
 def algorithm(draw, grid, start, end):
-    count=0
-    inQueue=[]
-    inQueue.append((count,start))
-    came_from={}
-    trav=[]
-    while( len(inQueue)!=0):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-        current=inQueue[0][1]
-        inQueue=inQueue[1:]
-        if current == end:
-            reconstruct_path(came_from,start, end, draw)
-            end.make_end()
-            return True
-        for neighbor in current.neighbors:
-            
-            if neighbor not in trav:
-                came_from[neighbor] = current
-                count += 1
-                inQueue.append((count,neighbor))
-                trav.append(neighbor)
-                neighbor.make_open()
-        draw()
-        if current != start:
-            current.make_closed()
-    for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-    return False
+	count = 0
+	open_set = PriorityQueue()
+	open_set.put((0, count, start))
+	came_from = {}
+	g_score = {spot: float("inf") for row in grid for spot in row}
+	g_score[start] = 0
+	f_score = {spot: float("inf") for row in grid for spot in row}
+	f_score[start] = h(start.get_pos(), end.get_pos())
+
+	open_set_hash = {start}
+
+	while not open_set.empty():
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+
+		current = open_set.get()[2]
 		
+		open_set_hash.remove(current)
+
+		if current == end:
+			reconstruct_path(came_from, end, draw)
+			end.make_end()
+			return True
+		for neighbor in current.neighbors:
+			temp_g_score = g_score[current] + 1
+			if temp_g_score < g_score[neighbor]:
+				came_from[neighbor] = current
+				g_score[neighbor] = temp_g_score
+				f_score[neighbor] = temp_g_score 
+				if neighbor not in open_set_hash:
+					count += 1
+					open_set.put((f_score[neighbor], count, neighbor))
+					open_set_hash.add(neighbor)
+					neighbor.make_open()
+		if current!=start:
+			current.make_complete()
+		#sleep(0.2)
+		draw()
+
+		if current != start:
+			current.make_closed()
+
+	return False
+
 
 def make_grid(rows, width):
 	grid = []
